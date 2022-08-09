24de58f8ac5b7a174d651b75feb80ce11b0d1834
scoobytux <tu.lna07@gmail.com>
2022-08-09 13:39:19 +0700

    feat: modify auth routes

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -1,12 +1,18 @@
 import { lazy } from "react";
+import { Navigate } from "react-router-dom";
 
-const Form = lazy(() => import("@/components/Form"));
+// Template
+const AuthTemplate = lazy(() => import("@/containers/AuthTemplate"));
+
+// Pages
+const LoginPage = lazy(() => import("@/containers/AuthTemplate/LoginPage"));
 
 const AuthRoutes = {
     path: "auth",
-    element: <Form />,
+    element: <AuthTemplate />,
     children: [
-        { path: "login", element: <div>Login</div> },
+        { path: "", element: <Navigate to="login" /> },
+        { path: "login", element: <LoginPage /> },
         { path: "signup", element: <div>Signup</div> },
     ],
 };
