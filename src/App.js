2eec505e5134eb7e6d05f4e15e2aae6c564b8513
scoobytux <tu.lna07@gmail.com>
2022-08-13 20:02:38 +0700

    vendor: add mui date picker dependencies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-//Route config
+// Route config
+import { Suspense } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "@/routes";
 
+// Datepicker
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+
 function App() {
     return (
-        <Router>
-            <Routes />
-        </Router>
+        <LocalizationProvider dateAdapter={AdapterMoment}>
+            <Suspense fallback={<div>loading ...</div>}>
+                <Router>
+                    <Routes />
+                </Router>
+            </Suspense>
+        </LocalizationProvider>
     );
 }
 
