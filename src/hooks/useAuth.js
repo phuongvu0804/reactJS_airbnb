e83fc336dcfa2d14bdabb320269a4d8a2c5642e2
scoobytux <tu.lna07@gmail.com>
2022-08-18 14:34:01 +0700

    fix: add function params + remove redundant code

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -3,15 +3,14 @@ import { useState } from "react";
 const useAuth = () => {
     const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
 
-    const login = () => {
-        user = { ...user, role: user.type };
+    const login = (user) => {
         localStorage.setItem("user", JSON.stringify(user));
         setUser(user);
     };
 
     const logout = () => {
         localStorage.removeItem("user");
-        setUser({});
+        setUser(null);
     };
 
     return { user, login, logout };
