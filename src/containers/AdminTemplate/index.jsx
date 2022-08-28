361721e2854887698bbc962d5d4c91e064376e91
scoobytux <tu.lna07@gmail.com>
2022-08-28 19:01:32 +0700

    style: add admin footer

--- src/containers/AdminTemplate/index.jsx
+++ src/containers/AdminTemplate/index.jsx
@@ -1,5 +1,8 @@
 import { Outlet } from "react-router-dom";
 
+// Material UI
+import { Stack, Link } from "@mui/material";
+
 // Components
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
@@ -14,6 +17,17 @@ const AdminTemplate = () => {
             <div className="admin-container">
                 <Navbar />
                 <Outlet />
+                <Stack component="footer" className="admin-footer" direction="row" spacing={2} justifyContent="center">
+                    <Link className="admin-footer-link" href="#" underline="none">
+                        &#169; Airbnb
+                    </Link>
+                    <Link className="admin-footer-link" href="#" underline="none">
+                        Contact
+                    </Link>
+                    <Link className="admin-footer-link" href="#" underline="none">
+                        Privacy & terms
+                    </Link>
+                </Stack>
             </div>
         </div>
     );
