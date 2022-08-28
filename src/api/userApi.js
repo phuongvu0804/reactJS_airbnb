be733a7580b0d2a1b6ae1e2b3b83ec902377af21
scoobytux <tu.lna07@gmail.com>
2022-08-28 18:30:38 +0700

    feat: add apis

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -3,9 +3,17 @@ import axiosClient from "./config/axiosClient";
 const resourceName = "users";
 
 const userApi = {
+    getUsers: () => {
+        const url = resourceName;
+        return axiosClient.get(url);
+    },
     getUserDetails: (id) => {
         const url = `${resourceName}/${id}`;
         return axiosClient.get(url);
+    },
+    deleteUser: (id) => {
+        const url = `${resourceName}/${id}`;
+        return axiosClient.delete(url);
     },
 };
 
