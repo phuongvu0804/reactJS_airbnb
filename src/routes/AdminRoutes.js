d52b4e22438bafa3dd9b96d31e4fb0cff74bb3ac
scoobytux <tu.lna07@gmail.com>
2022-08-28 15:55:36 +0700

    feat: add users route

--- src/routes/AdminRoutes.js
+++ src/routes/AdminRoutes.js
@@ -4,13 +4,16 @@ import { Navigate } from "react-router-dom";
 // Template
 const AdminTemplate = lazy(() => import("@/containers/AdminTemplate"));
 
+// Pages
+const UserManagementPage = lazy(() => import("@/containers/AdminTemplate/UserManagementPage"));
+
 const AdminRoutes = {
     path: "admin",
     element: <AdminTemplate />,
     children: [
         { path: "", element: <Navigate to="dashboard" /> },
         { path: "dashboard", element: <div>Dashboard</div> },
-        { path: "users", element: <div>Users</div> },
+        { path: "users", element: <UserManagementPage /> },
     ],
 };
 
