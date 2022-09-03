7fe19cc5852b8f77cce6b80e76308277af1863ab
scoobytux <tu.lna07@gmail.com>
2022-09-03 23:24:24 +0700

    style: restyle add new user page

--- src/containers/AdminTemplate/UserManagementPage/New/index.jsx
+++ src/containers/AdminTemplate/UserManagementPage/New/index.jsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 // Material UI
+import { Stack, Grid } from "@mui/material";
 import { DriveFolderUploadOutlined } from "@mui/icons-material";
+
+// Components
+import TextInput from "../../components/TextInput";
+import RadioInput from "../../components/RadioInput";
+import WaveSkeleton from "@/components/WaveSkeleton";
 
 // Style
 import "./style.scss";
@@ -9,49 +16,104 @@ import "./style.scss";
 export const inputs = [
     {
         id: 1,
-        label: "Username",
+        name: "name",
+        label: "Full Name",
         type: "text",
-        placeholder: "john_doe",
+        placeholder: "Le Nguyen Anh Tu",
     },
     {
         id: 2,
-        label: "Name and surname",
-        type: "text",
-        placeholder: "John Doe",
+        name: "email",
+        label: "Email",
+        type: "email",
+        placeholder: "tu.lna07@gmail.com",
     },
     {
         id: 3,
-        label: "Email",
-        type: "mail",
-        placeholder: "john_doe@gmail.com",
+        label: "Gender",
+        type: "radio",
     },
     {
         id: 4,
-        label: "Phone",
-        type: "text",
-        placeholder: "+1 234 567 89",
+        name: "birthday",
+        label: "Birthday",
+        type: "date",
     },
     {
         id: 5,
+        name: "password",
         label: "Password",
         type: "password",
     },
     {
         id: 6,
-        label: "Address",
+        name: "phone",
+        label: "Phone",
         type: "text",
-        placeholder: "Elton St. 216 NewYork",
+        placeholder: "0946688199",
     },
     {
         id: 7,
-        label: "Country",
+        name: "address",
+        label: "Address",
         type: "text",
-        placeholder: "USA",
+        placeholder: "170 St. Esdan, Wakanda, Earth",
     },
 ];
 
 const New = () => {
+    const { control, handleSubmit } = useForm({
+        reValidateMode: "onSubmit",
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+            phone: "",
+            birthday: "",
+            gender: true,
+            type: "ADMIN",
+            address: "",
+        },
+    });
     const [file, setFile] = useState("");
+
+    const renderInputs = () => {
+        return inputs.map((input) => {
+            const { id, name, label, type, placeholder } = input;
+            let inputNode = null;
+
+            if (0) {
+                inputNode = (
+                    <Stack spacing={0.8}>
+                        <WaveSkeleton variant="rectangular" sx={{ width: "40%", height: "12px" }} />
+                        <WaveSkeleton variant="rectangular" sx={{ width: "100%", height: "12px" }} />
+                    </Stack>
+                );
+            }
+
+            if (type === "text" || type === "email" || type === "password" || type === "date") {
+                inputNode = (
+                    <TextInput name={name} type={type} label={label} placeholder={placeholder} control={control} />
+                );
+            }
+
+            if (type === "radio") {
+                inputNode = <RadioInput label={label} />;
+            }
+
+            // Grid column size
+            const columnSize = {
+                address: 10.5,
+                default: 5,
+            };
+
+            return (
+                <Grid item xs={5} key={id}>
+                    {inputNode}
+                </Grid>
+            );
+        });
+    };
 
     return (
         <div className="new">
@@ -68,7 +130,7 @@ const New = () => {
                 </div>
                 <div className="right">
                     <form>
-                        <div className="form-input">
+                        {/* <div className="form-input">
                             <label htmlFor="file">
                                 Image: <DriveFolderUploadOutlined className="icon" />
                             </label>
@@ -84,8 +146,21 @@ const New = () => {
                                 <label>{input.label}</label>
                                 <input type={input.type} placeholder={input.placeholder} />
                             </div>
-                        ))}
-                        <button>Add</button>
+                        ))} */}
+                        {/* <Stack className="form-input">
+                            <InputLabel className="admin-form-input-label">Email</InputLabel>
+                            <TextInput name="email" type="email" className="admin-form-input" control={control} />
+                        </Stack>
+                        <Stack className="form-input" spacing={0.8}>
+                            <WaveSkeleton variant="rectangular" sx={{ width: "40%", height: "12px" }} />
+                            <WaveSkeleton variant="rectangular" sx={{ width: "100%", height: "12px" }} />
+                        </Stack> */}
+                        <Grid container columns={12} spacing={3} justifyContent="space-evenly">
+                            {renderInputs()}
+                            <Grid item xs={5}>
+                                <button>Add</button>
+                            </Grid>
+                        </Grid>
                     </form>
                 </div>
             </div>
