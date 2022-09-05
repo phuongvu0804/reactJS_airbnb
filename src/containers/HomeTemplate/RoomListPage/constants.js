a43871399706c764556813c027a66f64344a7b0f
phuongvu0804 <99994868+phuongvu0804@users.noreply.github.com>
2022-09-05 10:20:32 +0300

    Feat: add navigation by place types in Home page

---
+++ src/containers/HomeTemplate/RoomListPage/constants.js
@@ -0,0 +1,3 @@
+const modernCriteria = ["wifi", "hotTub", "pool", "dryer", "cableTV", "gym", "elevator"];
+
+export { modernCriteria };
--- src/containers/HomeTemplate/RoomListPage/index.jsx
+++ src/containers/HomeTemplate/RoomListPage/index.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 //Material UI
 import { Box } from "@mui/system";
-import { Button, Container, Grid, IconButton, Modal } from "@mui/material";
+import { Button, Container, Grid } from "@mui/material";
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
 
 //Components
@@ -12,18 +12,19 @@ import RoomFilterModal from "./components/RoomFilterModal";
 import RoomCard from "./components/RoomCard";
 
 //Others
-import Image from "@/components/Image";
 import "./style.scss";
 import { actCreateSave } from "@/store/actions/roomDetails";
 import { actGetRoomList } from "@/store/actions/roomList";
 import LoadMoreBtn from "@/components/LoadMoreBtn";
+import { modernCriteria } from "./constants";
+import { useCallback } from "react";
 
 function RoomListPage() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const locationId = useParams();
 
-    const roomList = useSelector((state) => state.roomList.roomList);
+    let roomList = useSelector((state) => state.roomList.roomList);
     let roomsSaved = useSelector((state) => state.roomDetails.roomSaved);
     const roomListLoading = useSelector((state) => state.roomList.loading);
     const locationLoading = useSelector((state) => state.locationList.loading);
@@ -32,13 +33,60 @@ function RoomListPage() {
     const [open, setOpen] = useState(false);
     const [dataLoading, setDataLoading] = useState(false);
     const [visible, setVisible] = useState(8);
+    const [totalRoom, setTotalRoom] = useState(0);
+
+    const filterRoomList = useCallback(() => {
+        switch (locationId.id) {
+            case "place-entire":
+                const entireList = roomList.filter((room) => room.guests >= 5);
+                // setTotalRoom(entireList.length);
+                return entireList;
+
+            case "place-modern":
+                const modernList = roomList.filter((room) => {
+                    for (let key of modernCriteria) {
+                        if (room[key]) {
+                            return room;
+                        }
+                    }
+                });
+                // setTotalRoom(modernList.length);
+                return modernList;
+
+            case "place-nature":
+                const natureList = roomList.filter((room) => {
+                    for (let key of modernCriteria) {
+                        if (room[key]) {
+                            return false;
+                        }
+                        return room;
+                    }
+                });
+                // setTotalRoom(natureList.length);
+                return natureList;
+
+            default:
+                // setTotalRoom(roomList.length);
+
+                return roomList;
+        }
+    }, [locationId.id]);
 
     useEffect(() => {
         if (!roomListLoading || !locationLoading) {
             setDataLoading(false);
         }
 
-        dispatch(actGetRoomList(locationId.id));
+        switch (locationId.id) {
+            case "place-entire":
+            case "place-modern":
+            case "place-nature":
+                dispatch(actGetRoomList());
+                break;
+
+            default:
+                dispatch(actGetRoomList(locationId.id));
+        }
     }, [locationId.id]);
 
     const handleOpen = () => setOpen(true);
@@ -74,8 +122,18 @@ function RoomListPage() {
         return className;
     };
 
+    const renderRoomCardList = () => {
+        const data = filterRoomList();
+        return data
+            ?.slice(0, visible)
+            .map((room) => (
+                <RoomCard key={room._id} room={room} handleLikeClass={handleLikeClass} handleLike={handleLike} />
+            ));
+    };
+
     return (
         <div id="room-list-page">
+            {console.log("render...")}
             {dataLoading && (
                 <>
                     <Container maxWidth="lg" className="room-list__head">
@@ -109,7 +167,7 @@ function RoomListPage() {
                 <>
                     <Container maxWidth="lg" className="room-list__head">
                         <div className="room-list__title-wrapper">
-                            <span className="room-list__room-number">Over 1,000 stays</span>
+                            <span className="room-list__room-number">Over 1000 stays</span>
                             <h3 className="page__main-title room-list__title">Accomodation in your selected area</h3>
                         </div>
                         <Button
@@ -124,21 +182,13 @@ function RoomListPage() {
                     <Container maxWidth="lg" className="room-list__content">
                         {error && <p>{error}</p>}
                         <Grid container spacing={2}>
-                            {roomList?.slice(0, visible).map((room) => (
-                                <RoomCard
-                                    key={room._id}
-                                    room={room}
-                                    handleLikeClass={handleLikeClass}
-                                    handleLike={handleLike}
-                                />
-                            ))}
+                            {renderRoomCardList()}
                         </Grid>
                         <Box sx={{ width: "100%", display: "flex" }}>
-                            {visible < roomList && (
+                            {visible < roomList.length && (
                                 <LoadMoreBtn
                                     className="room-list_load-button"
                                     variant="outlined"
-                                    sx={{ mt: "30px", mx: "auto" }}
                                     setVisible={setVisible}
                                     loadNumber={8}
                                 >
