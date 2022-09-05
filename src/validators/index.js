c9f61d89474fbad38a34d47471ecfa99b52d8302
scoobytux <tu.lna07@gmail.com>
2022-09-05 12:43:33 +0700

    feat: add user validator for admin page

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,4 +1,5 @@
 import filterSchema from "./filterValidator";
 import { loginSchema, signupSchema } from "./authValidator";
+import { userSchema, locationSchema, roomSchema } from "./adminValidator";
 
-export { filterSchema, loginSchema, signupSchema };
+export { filterSchema, loginSchema, signupSchema, userSchema, locationSchema, roomSchema };
