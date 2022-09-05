9259ef7d6f159bd66705f51bba8df436565c6d5c
scoobytux <tu.lna07@gmail.com>
2022-09-05 11:56:38 +0700

    src/validators/messages/index.js

--- src/validators/authValidator.js
+++ src/validators/authValidator.js
@@ -2,18 +2,24 @@ import * as yup from "yup";
 import pattern from "./pattern";
 import msg from "./messages";
 
-// Log in
+/*
+ * Log in
+ */
 const loginSchema = yup.object({
     email: yup.string().required(msg.required).email(msg.email),
     password: yup.string().matches(pattern.password, msg.password),
 });
 
-// Sign up
+/*
+ * Sign up
+ */
+const yesterday = new Date(new Date().setDate(new Date().getDate() - 1));
+
 const signupSchema = yup.object({
     name: yup.string().required(msg.required),
     email: yup.string().required(msg.required).email(msg.email),
     gender: yup.boolean(),
-    birthday: yup.string().required(msg.required),
+    birthday: yup.date().nullable().typeError(msg.birthday.invalid).max(yesterday, msg.birthday.max),
     password: yup.string().matches(pattern.password, msg.password),
     phone: yup.string().required(msg.required).matches(pattern.phone, msg.phone),
     address: yup.string().required(msg.required),
