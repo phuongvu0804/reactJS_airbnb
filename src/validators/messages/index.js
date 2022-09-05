605106aae0a376ec23fca9bacf39460e6447e041
scoobytux <tu.lna07@gmail.com>
2022-09-05 12:41:39 +0700

    fix: add dot at end line of message

--- src/validators/messages/index.js
+++ src/validators/messages/index.js
@@ -1,12 +1,12 @@
 const msg = {
-    number: "This field needs to be number",
+    number: "This field needs to be number.",
     birthday: { invalid: "Birthday is invalid.", max: "Birthday should be before current date." },
-    positiveNumber: "This field needs to be bigger than 0",
-    maxPrice: "This field needs to be smaller than 1 000 000 000",
+    positiveNumber: "This field needs to be bigger than 0.",
+    maxPrice: "This field needs to be smaller than 1 000 000 000.",
     required: "This field is required.",
     name: "Full name is invalid.",
     email: "Email is invalid.",
-    phone: "Phone number is invalid",
+    phone: "Phone number is invalid.",
     password: "Password must be minimum eight characters, at least one letter, one number and one special character.",
 };
 
