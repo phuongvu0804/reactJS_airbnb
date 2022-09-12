3955f2675b4b2b79865fed5cfd2991bf45d13382
scoobytux <tu.lna07@gmail.com>
2022-09-12 13:16:30 +0700

    feat: add location apis

--- src/api/locationApi.js
+++ src/api/locationApi.js
@@ -15,6 +15,22 @@ const locationApi = {
         const url = resourceName + `/by-valueate?valueate=${evaluation}`;
         return axiosClient.get(url);
     },
+    getLocationDetails: (id) => {
+        const url = `${resourceName}/${id}`;
+        return axiosClient.get(url);
+    },
+    addLocation: (location) => {
+        const url = resourceName;
+        return axiosClient.post(url, location);
+    },
+    updateLocation: (id, location) => {
+        const url = `${resourceName}/${id}`;
+        return axiosClient.put(url, location);
+    },
+    updateLocationPhoto: (id, photo) => {
+        const url = `${resourceName}/upload-images/${id}`;
+        return axiosClient.post(url, photo);
+    },
     deleteLocation: (id) => {
         const url = `${resourceName}/${id}`;
         return axiosClient.delete(url);
