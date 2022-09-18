7c9ea457f5909bbc0be011314ef173ab6a835ca8
phuongvu0804 <99994868+phuongvu0804@users.noreply.github.com>
2022-09-18 19:16:09 +0300

    Chore: add editing modal with responsive

--- src/containers/HomeTemplate/Profile/components/ProfileModal/index.jsx
+++ src/containers/HomeTemplate/Profile/components/ProfileModal/index.jsx
@@ -30,25 +30,14 @@ function ProfileModal({ open, handleClose }) {
     const [serverResponse, setServerResponse] = useState(null);
     const [birthday, setBirthday] = useState(null);
 
-    const style = {
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        width: "50%",
-        bgcolor: "background.paper",
-        boxShadow: 24,
-        p: 4,
-    };
-
     const handleBirthday = (newValue) => {
         setBirthday(newValue);
     };
 
     return (
         <Modal open={open} onClose={handleClose}>
-            <Box id="profile-modal" sx={style}>
-                <CloseBtn />
+            <Box id="profile-modal">
+                <CloseBtn onClick={handleClose} />
                 <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ my: "14px" }}>
                     Edit profile
                 </Typography>
@@ -59,31 +48,31 @@ function ProfileModal({ open, handleClose }) {
                 )} */}
                 {/* sx={{ display: "flex", flexDirection: "column" }} */}
                 <Grid container spacing={2} component="form">
-                    <Grid item md={6}>
+                    <Grid item xs={12} md={12} lg={6}>
                         <FormControl fullWidth className="profile-modal__input-group">
                             <FormLabel htmlFor="userName">Name</FormLabel>
                             <TextField variant="outlined" id="userName" name="userName" />
                         </FormControl>
                     </Grid>
-                    <Grid item md={6}>
+                    <Grid item xs={12} md={12} lg={6}>
                         <FormControl fullWidth className="profile-modal__input-group">
                             <FormLabel htmlFor="email">Email</FormLabel>
                             <TextField variant="outlined" id="email" name="email" />
                         </FormControl>
                     </Grid>
-                    <Grid item md={6}>
+                    <Grid item xs={12} md={12} lg={6}>
                         <FormControl fullWidth className="profile-modal__input-group">
                             <FormLabel htmlFor="phone">Phone number</FormLabel>
                             <TextField variant="outlined" id="phone" name="phone" />
                         </FormControl>
                     </Grid>
-                    <Grid item md={6}>
+                    <Grid item xs={12} md={12} lg={6}>
                         <FormControl fullWidth className="profile-modal__input-group">
                             <FormLabel htmlFor="address">Address</FormLabel>
                             <TextField variant="outlined" id="address" name="address" />
                         </FormControl>
                     </Grid>
-                    <Grid item md={6}>
+                    <Grid item xs={12} md={12} lg={6}>
                         <FormControl fullWidth className="profile-modal__input-group">
                             <FormLabel htmlFor="birthday">Date of birth</FormLabel>
                             <DesktopDatePicker
@@ -95,7 +84,7 @@ function ProfileModal({ open, handleClose }) {
                             />
                         </FormControl>
                     </Grid>
-                    <Grid item md={6}>
+                    <Grid item xs={12} md={12} lg={6}>
                         <FormControl fullWidth className="profile-modal__input-group">
                             <FormLabel htmlFor="gender">Gender</FormLabel>
                             <RadioGroup id="gender" name="gender">
