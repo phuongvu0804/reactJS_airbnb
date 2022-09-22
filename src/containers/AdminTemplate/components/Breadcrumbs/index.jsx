17bd768b62a37f27c1ecedbc9a075f1518a55aa1
scoobytux <tu.lna07@gmail.com>
2022-09-22 15:09:28 +0700

    fix: modify breadcrumbs

--- src/containers/AdminTemplate/components/Breadcrumbs/index.jsx
+++ src/containers/AdminTemplate/components/Breadcrumbs/index.jsx
@@ -1,23 +1,44 @@
-import { useNavigate, useLocation, useParams, useSearchParams } from "react-router-dom";
+import { useNavigate, useLocation, useParams } from "react-router-dom";
 
 // Material UI
 import { Stack, Typography, Breadcrumbs as MuiBreadcrumbs, Link } from "@mui/material";
 import { HomeOutlined } from "@mui/icons-material";
 
+const subpath = {
+    users: {
+        title: "users",
+        prevLevel: "",
+        nextLevel: "",
+    },
+    locations: {
+        title: "locations",
+        prevLevel: "",
+        nextLevel: "rooms",
+    },
+    rooms: {
+        title: "rooms",
+        prevLevel: "locations",
+        nextLevel: "",
+    },
+};
+
 const Breadcrumbs = () => {
     const navigate = useNavigate();
-    const [searchParams, _] = useSearchParams();
-    const { pathname, state } = useLocation();
+    const { pathname } = useLocation();
     const { id } = useParams();
 
-    const subpaths = pathname.split("/").slice(1);
+    const subpaths = pathname.split("/").slice(2);
     // Remove id in subpaths
     if (id === subpaths.at(-1)) {
         subpaths.pop();
     }
-    let root = "/" + subpaths.shift();
-    if (state?.prevPath && searchParams.has(state.prevPath.slice(0, -1))) {
-        subpaths.unshift(state.prevPath);
+
+    while (true) {
+        if (!subpath[subpaths[0]]?.prevLevel) {
+            break;
+        }
+
+        subpaths.unshift(subpath[subpaths[0]]?.prevLevel);
     }
 
     return (
@@ -36,7 +57,7 @@ const Breadcrumbs = () => {
                     return (
                         <Link
                             key={idx}
-                            onClick={() => navigate(1 - idx - subpaths.length)}
+                            onClick={() => navigate(1 + idx - subpaths.length)}
                             style={{ textDecoration: "none", cursor: "pointer", color: "var(--lightgray)" }}
                         >
                             {subpath}
